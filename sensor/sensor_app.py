8d8ace9518a5e1785fd6c6e839d84433bb899c0d
K.Sriyesh <sriyesh1999@users.noreply.github.com>
2020-07-01 03:50:46 +0530

    K

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,10 +1,24 @@
 # Runner script for all modules
 from load_data import load_sensor_data
+# Runner script for all modules
+from house_info import HouseInfo
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
 data = []
 data=load_sensor_data()
+
+
+##############################
+# Do not remove these two lines
+# They are needed to validate your unittest
+
+house_info=HouseInfo(data)
+test_area=1
+recs=house_info.get_data_by_area()
+house_info.get_data_by_area("id",rec_area=test_area)
+
+##############################
 print("Sensor Data App")
 print("Loaded records:{}".format(len(data)))
 ##############################
@@ -13,6 +27,7 @@ print("Loaded records:{}".format(len(data)))
 
 # Module 2 code here:
 
+print("\nHouse sensor records for area {}:{}".format(test_area,len(recs)))
 # Module 3 code here:
 
 # Module 4 code here:
