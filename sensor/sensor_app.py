40cd30b7885f43eb6e49538f1243c2a9f54542b3
K.Sriyesh <sriyesh1999@users.noreply.github.com>
2020-07-01 01:37:23 +0530

    app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,7 +3,7 @@ from load_data import load_sensor_data
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
-data = load_sensor_data()
+data = list(load_sensor_data())
 print("Sensor Data App")
 print("Loaded records:{}".format(len(data)))
 ##############################
